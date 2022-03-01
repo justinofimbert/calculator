0c4b5bb013e88ed59f46c827f9f2c2a323b19959
justinofimbert <justinofimbert@gmail.com>
2022-03-01 18:36:21 -0300

    Created operator function
    the operator function takes the name of an operator and two numeric
    values, and calls one of the four base functions on those two values.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function division(a, b) {
-  return a / b
+  return a / b;
 }
 
 function product(a, b) {
-  return a * b
+  return a * b;
+}
+
+function operate(operator, a, b) {
+  if (operator === "add") return add(a, b);
+  else if (operator === "subtract") return subtract(a, b);
+  else if (operator === "division") return division(a, b);
+  return product(a, b);
 }

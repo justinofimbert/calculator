a9a92b7fb9a6f0b2164162795df324cbd77635dd
justinofimbert <justinofimbert@gmail.com>
2022-03-02 21:33:49 -0300

    Add roundToTwo function to output, add comments
    roundToTwo function added so decimals don't overflow calculator's screen
    it was also implemented in the return statements of the operate function
    
    added comments to state basic functionality

--- script.js
+++ script.js
@@ -14,33 +14,43 @@ function product(a, b) {
   return a * b;
 }
 
+function roundToTwo (number) {
+  return +(Math.round(number + "e+2") + "e-2");
+}
+
 function operate(a, operator, b) {
-  if (operator === "+") return add(a, b);
-  else if (operator === "-") return subtract(a, b);
-  else if (operator === "x") return product(a, b);
-  else if (operator === "/") return division(a, b);
+  if (operator === "+") return roundToTwo(add(a, b));
+  else if (operator === "-") return roundToTwo(subtract(a, b));
+  else if (operator === "x") return roundToTwo(product(a, b));
+  else if (operator === "/") return roundToTwo(division(a, b));
   return `a: ${a}, operator: ${operator}, b: ${b}`;
 }
 
 function callOperate() {
+  if (previousNumberAndOperator.textContent === "") return;
   // this function calls operate() on currentNumber and previousNumberAndOperator and cleans pN&O
   const numberAndOperator = previousNumberAndOperator.textContent.split(" ");
   currentNumber.textContent = operate(+numberAndOperator[0], numberAndOperator[1], +currentNumber.textContent);
   previousNumberAndOperator.textContent = "";
 }
 
+// display parts
 const currentNumber = document.querySelector("#current-number");
 const previousNumberAndOperator = document.querySelector("#previous-number-and-operator");
 
+// buttons
 const numberButtons = Array.from(document.querySelectorAll(".number"));
 const operatorButtons = Array.from(document.querySelectorAll("#operators button"));
 const resultButton = document.querySelector("#operate");
 
+// each time you click a number, the number will be concatenated to the currentNumber div
 numberButtons.forEach(number => number.addEventListener("click", () => {
   currentNumber.textContent = currentNumber.textContent + number.id;
 }))
 
 operatorButtons.forEach(operator => operator.addEventListener("click", () => {
+  // if an operator is clicked when we have both numeric and operator variables available
+  // then callOperate over those variables
   if (previousNumberAndOperator.textContent !== "") callOperate();
 
   // currentNumber + operator are sent to previousNumberAndOperator text content
